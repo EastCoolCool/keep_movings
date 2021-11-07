f9fb25cab62458fec6703ea61219cfc863b8fb06
lds <11209803@qq.com>
2021-11-07 21:35:12 +0800

    解决demo项目依赖问题

--- demo/src/test/java/com/lds/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/lds/demo/DemoApplicationTests.java
@@ -1,11 +1,8 @@
 package com.lds.demo;
 
-import com.google.common.collect.Maps;
-import com.lds.demo.resttemplate.Behavior;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
